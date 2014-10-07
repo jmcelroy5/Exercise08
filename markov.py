3dfbfcf7825af39e3c0be019a4f473ee7f791580
Hackbright Student <info@hackbrightacademy.com>
2014-10-07 16:30:42 -0700

    working version that selects first word

--- markov.py
+++ markov.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
 import sys
+import random
+import string
 
 def make_chains(corpus):
     """Takes an input text as a string and returns a dictionary of
     markov chains."""
     chains = {}
 
-    read_text = corpus.read().replace("--"," ")       # store corpus in one long string
-    word_list = read_text.split()
+    input_text = corpus.read()
+    clean_text = input_text.replace("--"," ").replace("_"," ")      # store corpus in one long string
+    word_list = clean_text.split()
 
     #Loop through giant list and assign keys and values to empty dict
     for i in range(len(word_list)-2):
@@ -19,13 +22,24 @@ def make_chains(corpus):
             chains[key] = [value]
         else:                       # If word pair is in dictionary (append to value list)
             chains[key].append(value)
-            
+
     return chains
 
 def make_text(chains):
 
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
+    for i in range(20):
+
+        value_list = chains.values()
+        rand_num = random.randrange(len(value_list)) # Generate a random number within the length of dictionary
+        if value_list[rand_num][0][0] in string.ascii_uppercase: # If the first letter of the first word is a capital letter
+            print value_list[rand_num][0]
+
+        # first_word =       # randomly select a word pair that begins with capital letter
+        # for key, value
+        #     print value
+
     return "Here's some random text."
 
 def main():
