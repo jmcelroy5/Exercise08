0b867a81e0b1bf7262cddbc271425386183f496c
Jessica McElroy <mcelroyjessica@gmail.com>
2014-10-10 14:45:48 -0700

    Added twitter API code and source text files

--- markov.py
+++ markov.py
@@ -3,6 +3,13 @@
 import sys
 import random
 import string
+import twitter
+import os
+
+# tapi = os.environ.get("TWITTER_API_KEY")
+# api = twitter.Api(tapi)
+
+api = twitter.Api([twitter access keys])    # Twitter access keys removed
 
 def make_chains(corpus1,corpus2,n):
     """Takes an input text as a string and returns a dictionary of
@@ -16,18 +23,17 @@ def make_chains(corpus1,corpus2,n):
 
     mashup = input_text1 + input_text2      # Combining two source texts
 
-    clean_text = mashup.replace("--"," ").replace("_"," ")      
+    clean_text = mashup.replace("--"," ").replace("_"," ").replace("[", " ").replace("]"," ").replace("/", " ").replace("\\", " ")
+
     word_list = clean_text.split() 
 
     # Loop through word list and create groupings (of n-length) 
-
     for i in range(len(word_list)-n):
         key_tuple = []
         for j in range(n):
             key_tuple.append(word_list[i+j])
 
         # Assign key and value to dictionary of markov chains
-        
         key = tuple(key_tuple)
         value = word_list[i+n]
         
@@ -42,20 +48,21 @@ def make_text(chains):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
 
-    keys_list = chains.keys()   # list of keys (tuples of n-length)
+    keys_list = chains.keys()   # List of keys, tuples of n-length
           
     not_found = True    # Test random keys until a value with capital letter is found  
     while not_found:
-        option = random.choice(keys_list)   # randomly selects a key from dictionary 
+        option = random.choice(keys_list)   # Randomly selects a key from dictionary 
        
-        if chains[option][0][0] in string.ascii_uppercase:
-            seed_key = option #
+        if chains[option][0][0] in string.ascii_uppercase:  # Chooses key if value is uppercase
+            seed_key = option   
             new_word = chains[seed_key] 
             not_found = False
     
     sentence = ""
 
-    while new_word[-1] not in ".?!":
+    # Generate tweet-length text
+    while len(sentence) <= 130:     
 
         # Get new word (value of key)
         new_word = chains[seed_key] 
@@ -72,7 +79,7 @@ def make_text(chains):
         new_key.append(new_word)
         seed_key = tuple(new_key)
 
-    return sentence
+    return sentence + "."
 
 def main():
     args = sys.argv
@@ -83,10 +90,12 @@ def main():
     corpus1 = open(input_file1)
     corpus2 = open(input_file2)
 
-    chain_dict = make_chains(corpus1,corpus2, n)
+    chain_dict = make_chains(corpus1,corpus2, n)  
     random_text = make_text(chain_dict)
 
     print random_text
+    api.PostUpdate(random_text)
+
 
 if __name__ == "__main__":
     main()
